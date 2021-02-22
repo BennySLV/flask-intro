5ac7c99c8f776b1a674864461215e67edebe0c86
Ben Silveston <bsilveston37@gmail.com>
2021-02-22 21:29:35 +0000

    Created and initialised the database.

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-@app.route("/")
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    def __repr__(self):
+        # returns a String for every new task element that has been created
+        return '<Task %r>' % self.id
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
