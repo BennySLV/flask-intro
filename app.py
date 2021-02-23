08b51e897c37ef7c4a732cfdf2a67a9881e442f2
Ben Silveston <bsilveston37@gmail.com>
2021-02-23 18:36:52 +0000

    Added the "delete" and "update" methods and imported the missing "redirect" package.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
@@ -33,6 +33,35 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+
+@app.route("/delete/<int:id>", methods=['GET', 'POST'])
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting that task"
+
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue updating your task"
+
+    else:
+        return render_template("update.html", task=task_to_update)
 
 
 if __name__ == "__main__":
